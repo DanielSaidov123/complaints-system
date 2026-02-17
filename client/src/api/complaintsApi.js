42f9def3b62bf260da6bdf507dc98e8d26af45f1
Daniel_Saidov <ddny3622@gmail.com>
2026-02-17 22:11:34 +0200

    add folder in claient

---
+++ client/src/api/complaintsApi.js
@@ -0,0 +1,8 @@
+export const loginAdmin = async (credentials) => {
+    const response = await fetch('/api/login', {
+        method: 'POST',
+        body: JSON.stringify(credentials),
+        headers: { 'Content-Type': 'application/json' }
+    });
+    return response.json(); 
+};
