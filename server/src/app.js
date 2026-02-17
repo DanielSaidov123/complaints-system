e9d207054f3205e4a827b59fd052065baff35c6e
Daniel_Saidov <ddny3622@gmail.com>
2026-02-17 21:25:26 +0200

    add

--- server/src/app.js
+++ server/src/app.js
@@ -1,5 +1,6 @@
 import express from "express";
 import dotenv from "dotenv";
+import cors from "cors";
 import { connectDB } from "./db/connect.js";
 import Complaints from "./routes/complaints.routes.js";
 import AuthRoutes from "./routes/auth.routes.js"; // <--- חדש
@@ -8,7 +9,7 @@ import { requireAdminAuth } from "./middlewares/requireAdminAuth.js"; // <--- ח
 dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 5000;
-
+app.use(cors());
 app.use(express.json());
 
 app.use((req, res, next) => {
